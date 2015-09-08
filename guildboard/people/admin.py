6ad8299da114c291d965843dee34faf443e147ab
Delaney Fish <fishdela@gmail.com>
2015-09-08 15:09:11 -0500

    Issue #11: Register notification class. Add User admin info.

--- guildboard/people/admin.py
+++ guildboard/people/admin.py
@@ -1,7 +1,21 @@
 from django.contrib import admin
-
-from .models import Gym, Lifter, Federation
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.models import User
+from .models import Gym, Lifter,Notification
 
 admin.site.register(Gym)
 admin.site.register(Lifter)
-admin.site.register(Federation)
+# admin.site.register(Federation)
+admin.site.register(Notification)
+
+
+class LifterInline(admin.StackedInline):
+    model = Lifter
+    can_delete = False
+
+
+class UserAdmin(UserAdmin):
+    inlines = (LifterInline,)
+
+admin.site.unregister(User)
+admin.site.register(User, UserAdmin)
